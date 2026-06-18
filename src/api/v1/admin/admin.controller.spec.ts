b0b35284c90ab98841e27a9de664352cd130265b
Haider Ali <haiderali.ravian@gmail.com>
2026-06-18 18:21:32 +0500

    Refactor Admin module by removing unused AdminService and related endpoints

--- src/api/v1/admin/admin.controller.spec.ts
+++ src/api/v1/admin/admin.controller.spec.ts
@@ -1,7 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TasksService } from '../tasks/tasks.service';
 import { AdminController } from './admin.controller';
-import { AdminService } from './admin.service';
 
 describe('AdminController', () => {
   let controller: AdminController;
@@ -9,10 +8,7 @@ describe('AdminController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AdminController],
-      providers: [
-        { provide: AdminService, useValue: {} },
-        { provide: TasksService, useValue: {} },
-      ],
+      providers: [{ provide: TasksService, useValue: {} }],
     }).compile();
 
     controller = module.get<AdminController>(AdminController);
--- src/api/v1/admin/admin.controller.ts
+++ src/api/v1/admin/admin.controller.ts
@@ -1,14 +1,4 @@
-import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  Param,
-  Patch,
-  Post,
-  Query,
-  UseGuards,
-} from '@nestjs/common';
+import { Controller, Get, Query, UseGuards } from '@nestjs/common';
 import { ApiCookieAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { CurrentUser } from '../../../common/decorators/current-user.decorator';
 import { Roles } from '../../../common/decorators/roles.decorator';
@@ -23,17 +13,11 @@ import { ListTasksQueryDto } from '../tasks/dto/list-tasks-query.dto';
 import { TaskResponseDto } from '../tasks/dto/task-response.dto';
 import { TasksService } from '../tasks/tasks.service';
 import { User } from '../users/entities/user.entity';
-import { AdminService } from './admin.service';
-import { CreateAdminDto } from './dto/create-admin.dto';
-import { UpdateAdminDto } from './dto/update-admin.dto';
 
 @ApiTags('Admin')
 @Controller('admin')
 export class AdminController {
-  constructor(
-    private readonly adminService: AdminService,
-    private readonly tasksService: TasksService,
-  ) {}
+  constructor(private readonly tasksService: TasksService) {}
 
   @ApiOperation({
     summary: 'Admin test endpoint',
@@ -60,30 +44,5 @@ export class AdminController {
   @ApiErrorResponse(403, 'FORBIDDEN', 'Admin role required')
   listAllTasks(@Query() query: ListTasksQueryDto) {
     return this.tasksService.findAllAdmin(query);
-  }
-
-  @Post()
-  create(@Body() createAdminDto: CreateAdminDto) {
-    return this.adminService.create(createAdminDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.adminService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.adminService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAdminDto: UpdateAdminDto) {
-    return this.adminService.update(+id, updateAdminDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.adminService.remove(+id);
   }
 }
